e0ab94c22aaba583a23d312a86644b2d1b292548
TotoCode <totocodefr@duck.com>
2026-05-14 17:08:51 +0200

    nuit/moderation: add /unban command
    based off of /ban

--- src/modules/moderation/main.ts
+++ src/modules/moderation/main.ts
@@ -1,6 +1,9 @@
 import banCommandDetails from "./ban";
+import unbanCommandDetails from "./unban";
 import type { ModuleContext } from "@nuit-bot/api";
 
 export async function setup(ctx: ModuleContext) {
     await ctx.api.registerCommand(banCommandDetails);
+
+    await ctx.api.registerCommand(unbanCommandDetails);
 }
