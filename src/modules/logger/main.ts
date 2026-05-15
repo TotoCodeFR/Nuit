62d521dc461fd83e3f95595c5b4760fb14f9d667
TotoCode <totocodefr@duck.com>
2026-05-15 17:49:10 +0200

    nuit/messaging: add bus
    also add some test warnings to some if statements that used to return
    nothing

---
+++ src/modules/logger/main.ts
@@ -0,0 +1,30 @@
+import type { ModuleContext } from "@nuit-bot/api";
+import { SlashCommandBuilder } from "discord.js";
+
+declare module "@nuit-bot/api" {
+    interface MessageRegistry {
+        "logger:hello": {};
+    }
+}
+
+export async function setup(ctx: ModuleContext) {
+    await ctx.api.registerCommand({
+        data: new SlashCommandBuilder()
+            .setName("log")
+            .setDescription("Log a message with the message bus."),
+
+        async execute(interaction, ctx) {
+            if (!interaction.isChatInputCommand()) {
+                return;
+            }
+
+            await ctx.bus.emit("logger:hello", {});
+
+            return interaction.reply("hello");
+        },
+    });
+
+    await ctx.bus.on("logger:hello", (payload) => {
+        console.log("hello");
+    });
+}
