e2e9c8264f4141cf33ffefdcc474aaea0f1a398c
TotoCodeFR <totocodefr@duck.com>
2026-05-21 14:46:09 +0200

    feat(components): add Card component

--- src/web/pages/Test.tsx
+++ src/web/pages/Test.tsx
@@ -1,5 +1,6 @@
 import Button from "../components/Button";
 import ServerIcon from "../components/ServerIcon";
+import Card from "../components/Card";
 import "../styles/global.css";
 import "./Test.css";
 
@@ -22,6 +23,24 @@ export default function StyleTest() {
                 name="Hello"
             ></ServerIcon>
             <ServerIcon iconUrl="" name="Hello"></ServerIcon>
+            <h2>Cards</h2>
+            <div
+                style={{
+                    width: "400px",
+                    height: "300px",
+                    display: "flex",
+                    flexDirection: "column",
+                }}
+            >
+                <Card level={1} className="card--row">
+                    <ServerIcon
+                        name="Hello"
+                        iconUrl="https://images.unsplash.com/photo-1779332317860-ddf6f5ee74a2?w=48&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+                    ></ServerIcon>
+
+                    <h1>Hello</h1>
+                </Card>
+            </div>
         </div>
     );
 }
