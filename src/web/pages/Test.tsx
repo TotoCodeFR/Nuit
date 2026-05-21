9dbbcb4b4f9e9c84e018cfa908b1e468f3e4d9d1
TotoCodeFR <totocodefr@duck.com>
2026-05-21 14:15:36 +0200

    feat(components): add ServerIcon component

--- src/web/pages/Test.tsx
+++ src/web/pages/Test.tsx
@@ -1,4 +1,5 @@
 import Button from "../components/Button";
+import ServerIcon from "../components/ServerIcon";
 import "../styles/global.css";
 import "./Test.css";
 
@@ -15,6 +16,12 @@ export default function StyleTest() {
             <Button variant="primary" disabled={true}>
                 Disabled
             </Button>
+            <h2>Server Icon</h2>
+            <ServerIcon
+                iconUrl="https://images.unsplash.com/photo-1779332317860-ddf6f5ee74a2?w=48&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+                name="Hello"
+            ></ServerIcon>
+            <ServerIcon iconUrl="" name="Hello"></ServerIcon>
         </div>
     );
 }
