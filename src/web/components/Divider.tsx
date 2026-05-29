cea4b9da5a0abab755cdeade926f843ff8c3df08
TotoCode <totocodefr@duck.com>
2026-05-29 13:54:19 +0200

    feat(components): remove all components to add it to the components repo
    https://github.com/Nuit-Bot/Components

--- src/web/components/Divider.tsx
+++
@@ -1,9 +0,0 @@
-import "./Divider.css";
-
-type DividerProps = {
-    vertical?: boolean;
-};
-
-export default function Divider({ vertical = false }: DividerProps) {
-    return <div className={vertical ? "divider divider--vertical" : "divider"} />;
-}
--- src/web/components/FieldError.tsx
+++
@@ -1,9 +0,0 @@
-import "./FieldError.css";
-
-type FieldErrorProps = {
-    message: string;
-};
-
-export default function FieldError({ message }: FieldErrorProps) {
-    return <p className="fieldError">{message}</p>;
-}
